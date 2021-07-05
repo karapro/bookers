a4dc46886505b16873a74a2a7a05183da638e0ec
karapro <m01190872c@edu.teu.ac.jp>
2021-07-05 02:03:47 +0000

    サクセスメッセージの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,12 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    else
+      flash.now[:alert] = 'errors prohibited this book from being saved'
+      render :index
+    end
   end
 
   def show
@@ -20,18 +24,21 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+    end
   end
   
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      redirect_to books_path, notice: 'Book was successfully destroyed'
+    end
   end
   
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
 end
